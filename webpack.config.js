6539a9c4bd04d9a0ca05b962d55535ba05d037e0
nickfrahm <nfrahm24@gmail.com>
2021-05-23 16:22:13 -0500

    moved everything to dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,22 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   mode: "development",
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
+        type: "asset/resource",
+      },
+    ],
   },
 };
